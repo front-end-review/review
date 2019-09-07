570c6ec6d5c12cb7fb668eac68fbd57ea62853c6
chenyuhan1 <1944614333@qq.com>
2019-09-07 09:42:12 +0800

    readme

--- implementCode/test.js
+++ implementCode/test.js
@@ -1,1 +1,17 @@
-console.log(Symbol(1) === Symbol(1))
+// console.log(Symbol(1) === Symbol(1))
+
+function Person(name) {
+  this.name = name;
+  // return { test: '1'}
+  return function() {}
+}
+function Person1(name) {
+  this.name = name;
+  this.test = function() {
+    
+  }
+  return 1;
+}
+
+console.log(new Person('test')); // { test: '1'}
+console.log(new Person1('test')); // Person1 { name: 'test' }
